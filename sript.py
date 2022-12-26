aa7edb755abedab61ec4ff0a8b6194b8ef209c01
Trm888 <bigtram888@gmail.com>
2022-12-27 02:26:20 +0900

    Add function to collect dictionary with book information

--- sript.py
+++ sript.py
@@ -15,14 +15,38 @@ def check_for_redirect(response):
     if response.history:
         raise HTTPError('HTTP not found')
 
+
+def parse_book_page(book_id: int):
+    url = f'https://tululu.org/b{book_id}/'
+    response = requests.get(url)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    title_author_tag = soup.find('body').find('td', class_="ow_px_td").find('h1')
+    title_author_text = title_author_tag.text
+    title, author = title_author_text.split('::')
+    genres = soup.find('body').find('span', class_="d_book").find_all('a')
+    genres_list = []
+    for genre in genres:
+        genres_list.append(genre.text)
+    relative_url = soup.find('body').find_all('div', class_="texts")
+    comment_list = []
+    for comment in relative_url:
+        comment_list.append(comment.find('span').text)
+    book_information = {'Автор': author.strip(),
+                        'Заголовок': title.strip(),
+                        'Жанр': genres_list,
+                        'Комментарии': comment_list}
+    return book_information
+
 def parse_comments(book_id: int):
     url = f'https://tululu.org/b{book_id}/'
     response = requests.get(url)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
     relative_url = soup.find('body').find_all('div', class_="texts")
+    comment_list = []
     for comment in relative_url:
-        print(comment.find('span').text)
+        print(comment_list.append(comment.find('span').text))
 
 def parse_genres(book_id: int):
     url = f'https://tululu.org/b{book_id}/'
@@ -87,8 +111,6 @@ def save_image(book_id, image_url, folder='image/'):
 
 
 def main():
-    # env = Env()
-    # env.read_env()
     Path(os.getcwd(), 'books').mkdir(parents=True, exist_ok=True)
     book_qty = 10
     for book_id in range(1, book_qty + 1):
@@ -96,13 +118,10 @@ def main():
             parsed_book = parse_book(book_id)
             if not check_for_redirect(parsed_book):
                 filename = parse_name(book_id)
-                print(f'Заголовок: {filename}')
-                # parse_comments(book_id)
-                parse_genres(book_id)
                 image_url = parse_image(book_id)
                 save_image(book_id, image_url)
                 save_book(parsed_book, filename)
-
+                print(parse_book_page(book_id))
         except HTTPError:
             print('HTTP not found')
 
